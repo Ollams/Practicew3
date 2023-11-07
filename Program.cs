2efdf24c19556f96a755148136cf4590b20f08db
Olowoofayoku Sheriffdeen Olamilekan <olowoofayokusheriffdeen@gmail.com>
2023-11-07 16:28:12 +0100

    added new file

---
+++ Program.cs
@@ -0,0 +1,151 @@
+﻿// string name = "Ollams";
+// Console.WriteLine("Hello + Ollams");
+
+// string firstName = "Ollams";
+// string lastName = "Pictures";
+// string fullName = firstName + lastName;
+// Console.WriteLine(fullName);
+
+// int x = 5;
+// int y = 10;
+// Console.WriteLine(x +y);
+
+// int a = 10;
+// int b = 20;
+// int c = 30;
+// Console.WriteLine(a + b - c);
+
+// int myNum = 100000;
+// Console.WriteLine(myNum);
+
+// long myNum1 = 15000000000L;
+// Console.WriteLine(myNum);
+
+// float f1 = 35e3F;
+// double d1 = 12E4D;
+// Console.WriteLine(f1);
+// Console.WriteLine(d1);
+
+// string greetings = "Hello World!!!";
+// Console.WriteLine(greetings);
+
+// int sum1 = 100;
+// int sum2 = 200;
+// int sum3 = 300;
+// Console.WriteLine(sum1 + sum2);
+// Console.WriteLine(sum1 + sum3);
+// Console.WriteLine(sum1 + sum3 - sum2);
+// Console.WriteLine(sum2 * sum1);
+// Console.WriteLine(10% sum3);
+
+// string myString = "Hello";
+// Console.WriteLine(myString[3]);
+
+// int day = 4;
+// switch (day)
+// {
+//     case 1:
+//         Console.WriteLine("Monday");
+//         break;
+//     case 2:
+//         Console.WriteLine("Tuesday");
+//     case 3:
+//         Console.WriteLine("Wednesday");
+//     case 4:
+//         Console.WriteLine("Thursday");
+//     case 5:
+//         Console.WriteLine("Friday");
+//     case 6:
+//         Console.WriteLine("Saturday");
+//     case 7:
+//         Console.WriteLine("Sunday");
+//         break;
+// }
+
+// int i = 0;
+// while (i < 5)
+// {
+//     Console.WriteLine(i);
+//     i++;
+// }
+
+// for (int i = 0; i < 5; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+// for (int i = 0; i <= 10; i = i + 2)
+// {
+//     Console.WriteLine(i);
+// }
+
+// string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+// foreach (string i in cars)
+// {
+//     Console.WriteLine(i);
+// }
+
+// for (int i = 0; i < 10; i++)
+// {
+//     if (i == 4)
+//     {
+//         break;
+//     }
+//     Console.WriteLine(i);
+// }
+
+// for (int i = 0; i < 10; i++)
+// {
+//     if (i == 4)
+//     {
+//         continue;
+//     }
+//     Console.WriteLine(i);
+// }
+
+// 
+// for eachloop
+// string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+// foreach (string i in cars)
+// {
+//     Console.WriteLine(i);
+// }
+
+// Sort a string
+
+// string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+// Array.Sort(cars);
+// foreach (string i in cars)
+// {
+//     Console.WriteLine(i);
+// }
+
+// static void MyMethod()
+// {
+//     Console.WriteLine("My name is Ollams and I am a software developer!");
+// }
+
+// MyMethod();
+
+
+static void MyName(string firstName)
+{
+    Console.WriteLine(firstName + "Abdul-Lateef");
+}
+static void Main(String[] args)
+{
+    MyName("Sheriffdeen");
+    MyName("Olamilekan");
+    MyName("Ollams");
+}
+
+
+
+
+
+
+
+
+
+
+
